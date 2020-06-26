3dc6968778c7cce2445983e6faeec11733cfbe33
Roman Kornev <romanius@me.com>
2020-06-26 18:47:42 +0600

    Refactored code, added notify ty sysfs

--- wiegand-gpio.c
+++ wiegand-gpio.c
@@ -22,22 +22,16 @@
 #define MAX_WIEGAND_BYTES 6
 #define MIN_PULSE_INTERVAL_USEC 700
 
-#define GPIO_WIEGAND_D0 6
-#define GPIO_WIEGAND_D1 16
+#define GPIO_WIEGAND_D0 111
+#define GPIO_WIEGAND_D1 109
 static struct wiegand
 {
-  int startParity;
   char buffer[MAX_WIEGAND_BYTES];
   int currentBit;
 
-
   int readNum;
-  unsigned int lastFacilityCode;
-  unsigned int lastCardNumber;
-  bool lastDecoded;
   char lastBuffer[MAX_WIEGAND_BYTES];
   int numBits;
-
 }
 wiegand;
 
@@ -78,7 +72,6 @@ static ssize_t wiegandShow(
 	static char wiegand_buf[MAX_WIEGAND_BYTES * 8];
 	print_wiegand_data(wiegand_buf, wiegand.lastBuffer, wiegand.numBits);
 
-
   return sprintf(
     buf, "%.5d:%s\n",
     wiegand.readNum,
@@ -107,100 +100,33 @@ irqreturn_t wiegand_data_isr(int irq, void *dev_id);
 void wiegand_clear(struct wiegand *w)
 {
   w->currentBit = 0;
-  w->startParity = 0;
   memset(w->buffer, 0, MAX_WIEGAND_BYTES);
 }
 
 void wiegand_init(struct wiegand *w)
 {
-  w->lastFacilityCode = 0;
-  w->lastCardNumber = 0;
   w->readNum = 0;
   wiegand_clear(w);
 }
 
-//returns true if even parity
-bool checkParity(char *buffer, int numBytes, int parityCheck)
-{
-  int byte = 0;
-  int bit = 0;
-  int mask;
-  int parity = parityCheck;
-
-  for (byte = 0; byte < numBytes; byte++)
-  {
-    mask = 0x80;
-    for (bit = 0; bit < 8; bit++)
-    {
-      if (mask & buffer[byte])
-      {
-        parity++;
-      }
-      mask >>= 1;
-    }
-  }
-  return (parity % 2) == 1;
-}
-
-
 void wiegand_timer(unsigned long data)
 {
-  char *lcn;
   char buf[MAX_WIEGAND_BYTES * 8];
   size_t i;
   struct wiegand *w = (struct wiegand *) data;
   int numBytes = ((w->currentBit -1) / 8 )+ 1;
-  //~ int endParity = w->buffer[w->currentBit / 8] & (0x80 >> w->currentBit % 8);
 
-  printk("wiegand read complete\n");
+  for (i=0; i< numBytes; ++i){
+      w->lastBuffer[i] = w->buffer[i];
+  }
 
-
-
-	for (i=0; i< numBytes; ++i){
-		w->lastBuffer[i] = w->buffer[i];
-	}
-
-	w->numBits = w->currentBit;
-	w->lastDecoded = false;
+  w->numBits = w->currentBit;
   w->readNum++;
 
-	print_wiegand_data(buf, w->buffer, w->numBits);
-  printk("new read available [%d]: %s\n",  w->numBits, buf);
+  print_wiegand_data(buf, w->buffer, w->numBits);
+  printk("wiegand-gpio: new read available [%d]: %s\n",  w->numBits, buf);
 
-
-
-
-  //check the start parity
-  //~ if (checkParity(w->buffer, numBytes, w->startParity))
-  //~ {
-    //~ printk("start parity check failed\n");
-    //~ wiegand_clear(w);
-    //~ return;
-  //~ }
-//~
-  //~ //check the end parity
-  //~ if (!checkParity(&w->buffer[numBytes], numBytes, endParity))
-  //~ {
-    //~ printk("end parity check failed\n");
-    //~ wiegand_clear(w);
-    //~ return;
-  //~ }
-
-
-  //~ w->lastDecoded = true;
-  //~ //ok all good set facility code and card code
-  //~ w->lastFacilityCode = (unsigned int)w->buffer[0];
-//~
-  //~ //note relies on 32 bit architecture
-  //~ w->lastCardNumber = 0;
-  //~ lcn = (char *)&w->lastCardNumber;
-  //~ lcn[0] = w->buffer[2];
-  //~ lcn[1] = w->buffer[1];
-//~
-  //~ printk(
-    //~ "decoded data: %d:%d\n",
-    //~ w->lastFacilityCode,
-    //~ w->lastCardNumber);
+  sysfs_notify(wiegandKObj, NULL, "read");
 
 
   //reset for next reading
@@ -213,10 +139,9 @@ int init_module()
 {
   int retval, ret;
 
-  printk("wiegand intialising\n");
+  printk("wiegand-gpio: intialising\n");
 
   wiegand_init(&wiegand);
-
 
   ret = gpio_request(GPIO_WIEGAND_D0, "wiegand-d0");
   if (ret)
@@ -237,13 +162,13 @@ int init_module()
 
 	irq_d0 = gpio_to_irq(GPIO_WIEGAND_D0);
 	if (irq_d0 < 0) {
-		printk("can't request irq for D0 gpio\n");
+		printk("wiegand-gpio: can't request irq for D0 gpio\n");
 		return irq_d0;
 	}
 
 	irq_d1 = gpio_to_irq(GPIO_WIEGAND_D1);
 	if (irq_d1 < 0) {
-		printk("can't request irq for D1 gpio\n");
+		printk("wiegand-gpio: can't request irq for D1 gpio\n");
 		return irq_d1;
 	}
 
@@ -251,13 +176,13 @@ int init_module()
   /** Request IRQ for pin */
   if(request_any_context_irq(irq_d0, wiegand_data_isr, IRQF_SHARED | IRQF_TRIGGER_FALLING, "wiegand_data", &wiegand))
   {
-    printk(KERN_DEBUG"Can't register IRQ %d\n", irq_d0);
+    printk(KERN_DEBUG"wiegand-gpio: Can't register IRQ %d\n", irq_d0);
     return -EIO;
   }
 
   if(request_any_context_irq(irq_d1, wiegand_data_isr, IRQF_SHARED | IRQF_TRIGGER_FALLING, "wiegand_data", &wiegand))
   {
-    printk(KERN_DEBUG"Can't register IRQ %d\n", irq_d1);
+    printk(KERN_DEBUG"wiegand-gpio: Can't register IRQ %d\n", irq_d1);
     return -EIO;
   }
 
@@ -266,7 +191,7 @@ int init_module()
 
   if (!wiegandKObj)
   {
-    printk("wiegand failed to create sysfs\n");
+    printk("wiegand-gpio: failed to create sysfs\n");
     return -ENOMEM;
   }
 
@@ -282,7 +207,7 @@ int init_module()
   timer.data = (unsigned long) &wiegand;
 
 
-  printk("wiegand ready\n");
+  printk("wiegand-gpio: ready\n");
   return retval;
 }
 
@@ -296,28 +221,16 @@ irqreturn_t wiegand_data_isr(int irq, void *dev_id)
   getnstimeofday(&ts);
   interval = timespec_sub(ts,lastts);
   lastts = ts;
+
   if ((interval.tv_sec == 0 ) && (interval.tv_nsec < MIN_PULSE_INTERVAL_USEC * 1000)) {
 	  return IRQ_HANDLED;
   }
 
-
-
-//~
-  //~ int data0 = gpio_get_value(GPIO_WIEGAND_D0);
-  //~ int data1 = gpio_get_value(GPIO_WIEGAND_D1);
-  //~ int value = ((data0 == 1) && (data1 == 0)) ? 0x80 : 0;
-
-  //~ if ((data0 == 1) && (data1 == 1))
-  //~ { //rising edge, ignore
-    //~ return IRQ_HANDLED;
-  //~ }
-
-  //stop the end of transfer timer
   del_timer(&timer);
 
-	if (w->currentBit <=  MAX_WIEGAND_BYTES * 8) {
-		w->buffer[(w->currentBit) / 8] |= (value >> ((w->currentBit) % 8));
-	}
+  if (w->currentBit <=  MAX_WIEGAND_BYTES * 8) {
+      w->buffer[(w->currentBit) / 8] |= (value >> ((w->currentBit) % 8));
+  }
 
   w->currentBit++;
 
@@ -340,13 +253,9 @@ void cleanup_module()
   gpio_free(GPIO_WIEGAND_D0);
   gpio_free(GPIO_WIEGAND_D1);
 
-
-
-  printk("wiegand removed\n");
+  printk("wiegand-gpio: removed\n");
 }
 
 MODULE_DESCRIPTION("Wiegand GPIO driver");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("VerveWorks Pty. Ltd.");
-
-
